927476a40391b3ec90b3396e542af917f110dcdf
zhantong1994 <zhantong1994@163.com>
2016-03-19 17:20:43 +0800

    格式化代码
    代码太乱了...

--- functions.php
+++ functions.php
@@ -1,34 +1,38 @@
 <?php
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
-function themeConfig($form) {
-    $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点LOGO地址'), _t('在这里填入一个图片URL地址, 以在网站标题前加上一个LOGO'));
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
+function themeConfig($form)
+{
+    $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', null, null, _t('站点LOGO地址'), _t('在这里填入一个图片URL地址, 以在网站标题前加上一个LOGO'));
     $form->addInput($logoUrl);
 
     $db = Typecho_Db::get();
-    $pages=$db->fetchAll($db->select('slug,title')->from('table.contents')->where('type=?','page'));
-    $list=array('ShowRecentPosts' => _t('显示最新文章'),
+    $pages = $db->fetchAll($db->select('slug,title')->from('table.contents')->where('type=?', 'page'));
+    $list = array('ShowRecentPosts' => _t('显示最新文章'),
     'ShowRecentComments' => _t('显示最近回复'),
     'ShowCategory' => _t('显示分类'),
     'ShowArchive' => _t('显示归档'),
-    'ShowOther' => _t('显示其它杂项'));
+    'ShowOther' => _t('显示其它杂项'), );
     foreach ($pages as $key => $value) {
-      $pagesArray[$value['slug']] = $value['title'];
+        $pagesArray[$value['slug']] = $value['title'];
     }
-    $list=array_merge($list,$pagesArray);
-    $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock',$list,
-    array('ShowRecentPosts','ShowRecentComments','ShowCategory','ShowArchive','ShowOther'), _t('侧边栏显示'));
+    $list = array_merge($list, $pagesArray);
+    $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock', $list,
+    array('ShowRecentPosts', 'ShowRecentComments', 'ShowCategory', 'ShowArchive', 'ShowOther'), _t('侧边栏显示'));
     $form->addInput($sidebarBlock->multiMode());
-    $showPages = new Typecho_Widget_Helper_Form_Element_Checkbox('showPages',$pagesArray,array_keys($pagesArray), _t('独立页面显示'));
+    $showPages = new Typecho_Widget_Helper_Form_Element_Checkbox('showPages', $pagesArray, array_keys($pagesArray), _t('独立页面显示'));
     $form->addInput($showPages->multiMode());
 }
 
 function PageToLinks($page)
 {
-    $content=$page->content;
-    $title=$page->title;
-    echo "<h3 class='widget-title'>".$title."</h3>";
-    $content=str_replace('<ul>', "<ul class='widget-list'>", $content);
+    $content = $page->content;
+    $title = $page->title;
+    echo "<h3 class='widget-title'>".$title.'</h3>';
+    $content = str_replace('<ul>', "<ul class='widget-list'>", $content);
     echo $content;
 }
 /*
--- postlist.php
+++ postlist.php
@@ -8,10 +8,11 @@
 $this->need('header.php'); ?>
 
 <article class="content">
-  <section class="post">
-    <ul class="listing">
-      <?php $this->widget('Widget_Contents_Post_Recent', 'pageSize=10000')->parse('<li>{year}-{month}-{day} : <a href="{permalink}">{title}</a></li>'); ?>
-    </ul>
-  </section>
+    <section class="post">
+        <ul class="listing">
+            <?php $this->widget('Widget_Contents_Post_Recent', 'pageSize=10000')->parse('<li>{year}-{month}-{day} : <a href="{permalink}">{title}</a></li>'); ?>
+        </ul>
+    </section>
 </article>
+
 <?php $this->need('footer.php'); ?>
